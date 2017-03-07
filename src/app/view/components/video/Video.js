1cd608a8b89e1940b7a8200893a3a6b3a66a2107
BuzzDev <buzztech@BuzzDevs-iMac.local>
2017-03-07 16:55:51 +0100

    video update

--- src/app/view/components/video/Video.js
+++ src/app/view/components/video/Video.js
@@ -4,6 +4,7 @@ const READYSTATECHANGE      = 'readystatechange';
 const LOADSTART             = 'loadstart';
 const LOADEDMETADATA        = 'loadedmetadata';
 const LOADEDDATA            = 'loadeddata';
+const ENDED                 = 'ended';
 
 
 export default class Video {
@@ -25,6 +26,11 @@ export default class Video {
     if(!options.el) this.initialize(this._video, options);
     else this.addListeners(this._video);
 
+    this.onLoadStart = this.onLoadStart.bind(this);
+    this.onLoadedMetadata = this.onLoadedMetadata.bind(this);
+    this.onProgress = this.onProgress.bind(this);
+    this.onProgress = this.onCanPlay.bind(this);
+
   }
 
   initialize(v, options) {
@@ -42,19 +48,23 @@ export default class Video {
 
   addListeners(v) {
 
-    v.addEventListener(LOADSTART,           this.onLoadStart.bind(this), false);
-    v.addEventListener(LOADEDMETADATA,      this.onLoadedMetadata.bind(this), false);
+    v.addEventListener(LOADSTART,           this.onLoadStart, false);
+    v.addEventListener(LOADEDMETADATA,      this.onLoadedMetadata, false);
+    v.addEventListener(PROGRESS,            this.onProgress, false);
   }
 
   onLoadStart(e) {
 
-    this._video.addEventListener(CANPLAY,             this.onCanPlay.bind(this), false);
+    this._video.addEventListener(CANPLAY,   this.onCanPlay, false);
   }
 
   onLoadedMetadata(e) {
     this.initProgress();
   }
 
+  onProgress(e) {
+
+  }
 
   initProgress() {
 
@@ -75,6 +85,22 @@ export default class Video {
 
   onReady() {
     //
+  }
+
+  play() {
+    this._video.play();
+  }
+
+
+  dispose() {
+
+    this._video.removeEventListener(LOADSTART, this.onLoadStart);
+    this._video.removeEventListener(LOADEDMETADATA, this.onLoadedMetadata);
+    this._video.removeEventListener(PROGRESS, this.onProgress);
+    this._video.removeEventListener(CANPLAY, this.onCanPlay);
+
+    this._video.setAttribute('src','');
+    this._video = null;
   }
 
   get buffer() {
